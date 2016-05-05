e65c852cfc7d89796d7f0f71c933b9bc0b01898d
Kyle Ricks <kyle.ricks@wwu.edu>
2016-05-05 14:28:40 -0700

    fixed find with -type f which allows code to function as designed

--- archive.sh
+++ archive.sh
@@ -4,16 +4,22 @@
 # Script to backup video from cameras in Computer Science Department.
 #
 # Written By: Kyle Ricks
+# Date: 20150505
 #
 ######################################################################
-
+#
+# To call: navigate to script and use 
+# 'sudo ./archive.sh /path/to/videos/'
+#
+#
 # Steps
 # 0) Arguments
 # 1) Delete non-avi
 # 2) Create Archive File
-# 3) Add all files older than 90 days to Archive
-# 4) Remove all files archived
-
+# 3a) Add each file older than 90 days to Archive
+# 3b) Remove each file after being added to the archive
+# 4) Compress archive with bzip2
+#
 ######################################################################
 #
 # Arguments
@@ -24,6 +30,12 @@ video_dir=$1
 DATE=$(date "+%Y-%m-%d")
 
 archivefile=video_archive.${DATE}.tar
+
+######################################################################
+#
+# Main Body
+#
+######################################################################
 
 # Delete all .jpg
 echo "Deleting all .jpg files in" $video_dir
@@ -36,7 +48,7 @@ tar -cf $video_dir/$archivefile $video_dir/start.txt && /bin/rm $video_dir/start
 
 # Append files to the archive
 echo "Adding video files to" $archivefile
-find $video_dir -mtime +90 -exec /usr/bin/tar -rf $video_dir/$archivefile {} \; -exec /bin/rm {} \;
+find $video_dir -type f -mtime +90 -exec /usr/bin/tar -rf $video_dir/$archivefile {} \; -exec /bin/rm {} \;
 
 # Bzip2 the Tar File
 echo "Compressing" $archivefile "with Bzip2.  This will take some time."
