e0c6e1098ef56cfaeb82912562caf60a4e539eb4
Kyle <cryocube@users.noreply.github.com>
2016-10-10 10:33:08 -0700

    log cleanup
    added timestamps for the log files

--- archive.sh
+++ archive.sh
@@ -4,7 +4,7 @@
 # Script to backup video from cameras in Computer Science Department.
 #
 # Written By: Kyle Ricks
-# Updated   : 20160923
+# Updated   : 20161010
 #
 ######################################################################
 
@@ -39,33 +39,34 @@ candidate_file=candidate_file_${DATE}.txt
 ######################################################################mv
 
 # Create Log File
-echo "Archive Script beginning" $DATE | tee $log_dir/$log_file
-echo "Creating Log File" $log_file | tee -a $log_dir/$log_file
+echo "["$date +%H:%M:%S)"] Archive Script beginning" $DATE | tee $log_dir/$log_file
+echo "["$date +%H:%M:%S)"] Creating Log File" $log_file | tee -a $log_dir/$log_file
 
 # Delete all .jpg
-echo "Deleting all .jpg files in" $video_dir | tee -a $log_dir/$log_file
+echo "["$date +%H:%M:%S)"] Deleting all .jpg files in" $video_dir | tee -a $log_dir/$log_file
 find $video_dir -name "*.jpg" -delete
 
 # Create File of Archive Candidates
-echo "Creating Archive Candidates File" | tee -a $log_dir/$log_file
+echo "["$date +%H:%M:%S)"] Creating Archive Candidates File" | tee -a $log_dir/$log_file
 touch $log_dir/$candidate_file
 find $video_dir -type f -mtime +30 | tee -a $log_dir/$candidate_file
 
 # Create Archive
-echo "Creating" $archivefile | tee -a $log_dir/$log_file 
+echo "["$date +%H:%M:%S)"] Creating" $archivefile | tee -a $log_dir/$log_file
 tar -cf $video_dir/$archivefile -T $log_dir/$candidate_file
 
 # Delete the candidates
-echo "Deleting Candidates from" $candidate_file  | tee -a $log_dir/$log_file
+echo "["$date +%H:%M:%S)"] Deleting Candidates from" $candidate_file  | tee -a $log_dir/$log_file
 xargs rm < $log_dir/$candidate_file
-
+                                                                                                                                                                                                 
 # Bzip2 the Tar File
-echo "Compressing" $archivefile "with Bzip2.  This will take some time." | tee -a $log_dir/$log_file
+echo "["$date +%H:%M:%S)"] Compressing" $archivefile "with Bzip2.  This will take some time." | tee -a $log_dir/$log_file
 bzip2 -9 $video_dir/$archivefile
 
 # Move completed .bz2 to transfer directory
-echo "Moving" ${archivefile}.bz2 "to" $transfer_dir | tee -a $log_dir/$log_file
+echo "["$date +H%:M%:%S)"] Moving" ${archivefile}.bz2 "to" $transfer_dir | tee -a $log_dir/$log_file
 mv $video_dir/${archivefile}.bz2 $transfer_dir
 
 # Program end - Terminate Log File
-echo "Script completed" | tee -a $log_dir/$log_file
+echo "["$date +H%:%M:%S)"] Script completed" | tee -a $log_dir/$log_file
+
